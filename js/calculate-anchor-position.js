2108729b53f7bb550771fce1db6df0ca27e2da6f
Ana <anamonroe@gmail.com>
2020-04-08 09:38:24 -0700

    updates to menu and side nav nested elements
    menu, sidenav behavior and color, content

--- js/calculate-anchor-position.js
+++ js/calculate-anchor-position.js
@@ -4,24 +4,16 @@ var $ = require('jquery');
  * show an anchor properly under the header
  * and lined up with the nav like the H1
  */
+
 var calculateAnchorPosition = function (hash) {
   var anchor        = $('#' + hash);
-  var topOffset     = 0;
   if (anchor.length === 0) {
     return topOffset;
   }
 
-  var navPadding    = parseInt($('.sidenav').css('padding-top'), 10);
-  var anchorPadding = parseInt(anchor.css('padding-top'), 10);
-
-  //start with the height of the header
-  topOffset = $('.site-nav-secondary').first().outerHeight();
-  //subtract the diffence in padding between nav top and anchor
-  topOffset = topOffset - (anchorPadding - navPadding);
 
   //anchor should now align with first item inside nav
-  return anchor.offset().top - topOffset;
+  return anchor.offset().top;
 };
-
 
 module.exports = calculateAnchorPosition;
---
+++ pages/home-sub.md
@@ -0,0 +1,27 @@
+---
+permalink: /home-sub/
+layout: styleguide
+title: HCD Discovery Concept Guide
+category: Welcome
+lead: This is some lede text
+subnav:
+- text: Theme tokens
+  href: '#theme-tokens'
+- text: System tokens
+  href: '#system-tokens'
+- text: Customizing tokens
+  href: '#customizing-tokens'
+- text: Using weight tokens
+  href: '#using-font-weight-tokens'
+
+---
+
+# This is a headline
+
+This is a paragraph
+
+## h2
+
+### h3
+
+#### h4
